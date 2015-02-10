dd58ea809df84df70caeaa7f179dcffcd823a734
Marc Felden <marcfelden@fsmn0041.fritz.box>
2015-02-10 05:52:27 +0100

    first version storyboard only

--- XCTTemperature/ViewController.swift
+++ XCTTemperature/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var textFieldFahrenheit: UITextField!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
